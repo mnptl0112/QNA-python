e824cc7d8de8d936e497933bec35e474aec2ea08
Mohammednaeem Patel <monaeem0112@gmail.com>
2025-04-21 19:10:01 -0400

    made something similar QNA

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 questions = ["What is your fav color?","how is the whether?","whats up?"]
 answers = ['black','good','nice']
+sum=0
+for que in questions:
+    ans = input(print(que))
+    for an in answers:
+        if ans == an:
+            print("correct answer")
+            sum +=10
+            break
+    else:
+        print('wrong answer')
+print(F'you have won {sum}, which is your!!\ncongrats!!')
